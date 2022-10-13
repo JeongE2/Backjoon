1c008a7b799ffa8723bf8c03ce0ecf77f6010db8
JeongE2 <kpo5248@ynu.kr>
2022-10-13 17:41:55 +0900

    hanoi

--- src/Problems/P1427/Main.java
+++ src/Problems/P1427/Main.java
@@ -5,28 +5,33 @@ import java.util.*;
 public class Main {
 
     static int N;
-    static int[] A, B, C;
+    static List<Integer> A = new ArrayList<>();
+    static List<Integer> B = new ArrayList<>();
+    static List<Integer> C = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("src/Problems/P1427/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 //        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(br.readLine());
-        A  = new int[N];
-        B  = new int[N];
-        C  = new int[N];
+        N = Integer.parseInt(br.readLine());
 
-        int j = 0;
-        for (int i = N; i > 0; i--) {
-            A[j++] = i;
+        for (int i = 1; i <= N; i++) { //List의 0가 젤 위
+            A.add(i);
         }
 
+        hanoi(A, C, B);
         //bw.write(sb+"\n");
         bw.close();
     }
 
-    static void hanoi(int from, int to, int tmp){
+    static void hanoi(List<Integer> from, List<Integer> to, List<Integer> tmp){
+        if(from.size()==1){
+            //옮기기
+        } else{
 
+        }
     }
+
+
 }
